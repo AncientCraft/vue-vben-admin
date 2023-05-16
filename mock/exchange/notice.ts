ca0c9d4b9f6c94f34cda86d99401a1f9ec252144
Ancient <ancientworldcraft@gmail.com>
2023-05-16 16:02:19 +0700

    feat: local dev can run

--- mock/exchange/notice.ts
+++ mock/exchange/notice.ts
@@ -1,36 +1,36 @@
-import { MockMethod } from 'vite-plugin-mock';
-import { resultPageSuccess } from '../_util';
+// import { MockMethod } from 'vite-plugin-mock';
+// import { resultPageSuccess } from '../_util';
 
-const demoList = (() => {
-  const result: any[] = [];
-  for (let index = 0; index < 2; index++) {
-    result.push({
-      key: `${index}`,
-      name: '@cname()',
-      list: notice,
-    });
-  }
-  return result;
-})();
+// const demoList = (() => {
+//   const result: any[] = [];
+//   for (let index = 0; index < 2; index++) {
+//     result.push({
+//       key: `${index}`,
+//       name: '@cname()',
+//       list: notice,
+//     });
+//   }
+//   return result;
+// })();
 
-const notice = (() => {
-  const result: any[] = [];
-  for (let index = 0; index < 30; index++) {
-    result.push({
-      id: `${index}`,
-      title: '@cname()',
-    });
-  }
-})();
+// const notice = (() => {
+//   const result: any[] = [];
+//   for (let index = 0; index < 30; index++) {
+//     result.push({
+//       id: `${index}`,
+//       title: '@cname()',
+//     });
+//   }
+// })();
 
-export default [
-  {
-    url: '/basic-api/notice/list',
-    timeout: 1000,
-    method: 'post',
-    response: ({ query }) => {
-      const { page = 1, pageSize = 20 } = query;
-      return resultPageSuccess(page, pageSize, demoList);
-    },
-  },
-] as MockMethod[];
+// export default [
+//   {
+//     url: '/basic-api/notice/list',
+//     timeout: 1000,
+//     method: 'post',
+//     response: ({ query }) => {
+//       const { page = 1, pageSize = 20 } = query;
+//       return resultPageSuccess(page, pageSize, demoList);
+//     },
+//   },
+// ] as MockMethod[];
